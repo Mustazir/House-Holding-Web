10cffc3b05ffedbb87c611302fd850e23edff6a3
mustazir <billah15-5193@diu.edu.bd>
2025-05-27 20:24:40 +0600

    navbar

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import img1 from './../assets/img1.png';
+import Navbar from './../components/Navbar';
 const Hero = () => {
     return (
       <div>
         <h1></h1>
+        <Navbar/>
         <div>
             <img src={img1} alt="" />
             <img src="" alt="" />
